b5988e4725642d2f23fc9770a3b3496ca7bfe190
Ary Sayd Sault <sault.sayd.ary@gmail.com>
2021-06-22 10:20:43 -0300

    Pagina Your Lista finalizada

--- lib/view/your_list.dart
+++ lib/view/your_list.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:movie_api/components/fav_movie.dart';
 import 'package:movie_api/components/native_loading.dart';
 import 'package:movie_api/model/results.dart';
@@ -17,6 +20,9 @@ class YourList extends StatefulWidget {
 
 class _YourListState extends State<YourList> {
   MovieViewModel _viewModel = MovieViewModel();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<LiquidPullToRefreshState> _refreshIndicatorKey =
+      GlobalKey<LiquidPullToRefreshState>();
   //String poster = "/xbSuFiJbbBWCkyCCKIMfuDCA4yV.jpg";
   @override
   void initState() {
@@ -27,6 +33,28 @@ class _YourListState extends State<YourList> {
   favsListen(movie) {
     _viewModel.favsStream.listen((event) async {
       await _viewModel.check(movie: movie);
+    });
+  }
+
+  Future<void> _handleRefresh() {
+    final Completer<void> completer = Completer<void>();
+    Timer(const Duration(seconds: 1), () {
+      completer.complete();
+    });
+    setState(() {
+      _viewModel.getAll();
+    });
+    return completer.future.then<void>((_) {
+      _scaffoldKey.currentState?.showSnackBar(
+        SnackBar(
+          content: const Text('Refresh complete'),
+          action: SnackBarAction(
+              label: 'RETRY',
+              onPressed: () {
+                _refreshIndicatorKey.currentState.show();
+              }),
+        ),
+      );
     });
   }
 
@@ -48,7 +76,7 @@ class _YourListState extends State<YourList> {
             children: [
               DrawerHeader(
                 child: Text(
-                  "ASS MAX",
+                  "PIRATA MAX",
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 26,
@@ -93,60 +121,72 @@ class _YourListState extends State<YourList> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 16.0),
-          child: StreamBuilder<BaseResponse<List<Results>>>(
-            stream: _viewModel.moviesStream,
-            initialData: BaseResponse.completed(),
-            builder: (context, snapshot) {
-              if (snapshot.data.data != null) {
-                switch (snapshot.data?.status) {
-                  case Status.LOADING:
-                    return _onLoading();
-                    break;
-                  case Status.ERROR:
-                    onError(snapshot);
-                    return Container();
-                    break;
-                  default:
-                    _viewModel.yourList.clear();
-                    if (snapshot.data.data != null) {
-                      snapshot.data.data.forEach((movie) {
-                        _viewModel.yourList.add(
-                          FavMovie(
-                            movie: movie,
-                            onPressed: () async {
-                              movie.id = movie.idAPI;
-                              await _viewModel.delete(movie: movie);
-                              favsListen(movie);
-                              print(movie.toJson());
-                            },
-                          ),
+      body: LiquidPullToRefresh(
+        key: _refreshIndicatorKey,
+        onRefresh: _handleRefresh,
+        showChildOpacityTransition: false,
+        animSpeedFactor: 1.2,
+        springAnimationDurationInMilliseconds: 700,
+        color: Color(0xff141E26),
+        backgroundColor: Colors.white,
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 16.0),
+              child: StreamBuilder<BaseResponse<List<Results>>>(
+                stream: _viewModel.moviesStream,
+                initialData: BaseResponse.completed(),
+                builder: (context, snapshot) {
+                  if (snapshot.data.data != null) {
+                    switch (snapshot.data?.status) {
+                      case Status.LOADING:
+                        return _onLoading();
+                        break;
+                      case Status.ERROR:
+                        onError(snapshot);
+                        return Container();
+                        break;
+                      default:
+                        _viewModel.yourList.clear();
+                        if (snapshot.data.data != null) {
+                          snapshot.data.data.forEach((movie) {
+                            _viewModel.yourList.add(
+                              FavMovie(
+                                movie: movie,
+                                onPressed: () async {
+                                  movie.id = movie.idAPI;
+                                  await _viewModel.delete(movie: movie);
+                                  favsListen(movie);
+                                  print(movie.toJson());
+                                },
+                              ),
+                            );
+                          });
+                        }
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: _viewModel.yourList.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                              child: _viewModel.yourList[index],
+                            );
+                          },
                         );
-                      });
                     }
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: _viewModel.yourList.length,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-                          child: _viewModel.yourList[index],
-                        );
-                      },
+                  } else {
+                    return Text(
+                      "LISTA BUGADA",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     );
-                }
-              } else {
-                return Text(
-                  "LISTA BUGADA",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                );
-              }
-            },
+                  }
+                },
+              ),
+            ),
           ),
         ),
       ),
